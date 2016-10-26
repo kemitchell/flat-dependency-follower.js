567be5773fdb51baa16f2bacc877234f2434ad2c
Kyle E. Mitchell <kyle@kemitchell.com>
2016-10-26 15:17:46 -0700

    Support ignoring specific packages by name

--- follower.js
+++ follower.js
@@ -11,6 +11,18 @@ var log = pino({
   level: process.env.LOG_LEVEL.toLowerCase() || 'warn'
 })
 
+var IGNORE = process.env.IGNORE
+  ? process.env.IGNORE
+    .split(',')
+    .map(function (string) {
+      return string.trim()
+    })
+  : false
+
+log.info({
+  packages: IGNORE
+}, 'ignoring')
+
 sequence(DIRECTORY, function (error, since) {
   if (error && error.code !== 'ENOENT') {
     log.error(error)
@@ -23,6 +35,16 @@ sequence(DIRECTORY, function (error, since) {
     }, 'start')
     pull(
       registry({since: since}),
+      pull.filter(function (update) {
+        var ignore = IGNORE && IGNORE.indexOf(update.id) !== -1
+        if (ignore) {
+          log.info({
+            sequence: update.seq,
+            id: update.id
+          }, 'ignored')
+        }
+        return !ignore
+      }),
       function loggingSpy (source) {
         return function (end, callback) {
           source(end, function (end, update) {
